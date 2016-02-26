f83e9370927b2807e939b36cbb771ec0a27b3565
flametuner <flame.tuner@gmail.com>
2016-02-26 16:16:54 -0300

    Adicionado UpdateEvent

---
+++ src/br/com/battlebits/ycommon/bukkit/event/UpdateEvent.java
@@ -0,0 +1,30 @@
+package br.com.battlebits.ycommon.bukkit.event;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class UpdateEvent extends Event{
+
+	public static final HandlerList handlers = new HandlerList();
+	private UpdateType type;
+
+	public UpdateEvent(UpdateType type) {
+		this.type = type;
+	}
+
+	public UpdateType getType() {
+		return type;
+	}
+
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+	public static enum UpdateType {
+		TICK, SECOND, MINUTE;
+	}
+}
---
+++ src/br/com/battlebits/ycommon/bukkit/event/UpdateScheduler.java
@@ -0,0 +1,25 @@
+package br.com.battlebits.ycommon.bukkit.event;
+
+import org.bukkit.Bukkit;
+
+import br.com.battlebits.ycommon.bukkit.event.UpdateEvent.UpdateType;
+
+public class UpdateScheduler implements Runnable {
+
+	private long lastSecond = Long.MIN_VALUE;
+	private long lastMinute = Long.MIN_VALUE;
+
+	@Override
+	public void run() {
+		Bukkit.getPluginManager().callEvent(new UpdateEvent(UpdateType.TICK));
+		if (lastSecond + 1000 <= System.currentTimeMillis()) {
+			Bukkit.getPluginManager().callEvent(new UpdateEvent(UpdateType.SECOND));
+			lastSecond = System.currentTimeMillis();
+		}
+		if (lastMinute + 60000 <= System.currentTimeMillis()) {
+			Bukkit.getPluginManager().callEvent(new UpdateEvent(UpdateType.MINUTE));
+			lastMinute = System.currentTimeMillis();
+		}
+	}
+
+}
