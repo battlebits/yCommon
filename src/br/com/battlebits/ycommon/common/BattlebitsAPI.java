6495b760b4d0af27b82b6efe26cb480e55880115
flametuner <flame.tuner@gmail.com>
2016-02-26 20:37:57 -0300

    Adicionado campos para website

--- src/br/com/battlebits/ycommon/common/BattlebitsAPI.java
+++ src/br/com/battlebits/ycommon/common/BattlebitsAPI.java
@@ -9,6 +9,13 @@ public class BattlebitsAPI {
 	private static AccountCommon accountCommon = new AccountCommon();
 	private static Logger logger = Logger.getLogger("Minecraft");
 	private static boolean debugMode = true;
+	public final static String FORUM_WEBSITE = "";
+	public final static String WEBSITE = "";
+	public final static String STORE = "";
+	public final static String ADMIN_EMAIL = "";
+	public final static String TWITTER = "";
+	public final static String HUNGERGAMES_ADDRESS = "";
+	
 
 	public static AccountCommon getAccountCommon() {
 		return accountCommon;
