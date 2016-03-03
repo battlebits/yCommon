b2f59fd190fd47876b5777f773f2cdd006876851
flametuner <flame.tuner@gmail.com>
2016-03-03 01:49:44 -0300

    Removido codigo inutilizado

--- src/br/com/battlebits/ycommon/bukkit/accounts/BukkitAccount.java
+++ src/br/com/battlebits/ycommon/bukkit/accounts/BukkitAccount.java
@@ -1,8 +1,5 @@
 package br.com.battlebits.ycommon.bukkit.accounts;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.Socket;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
@@ -11,8 +8,6 @@ import org.bukkit.entity.Player;
 import br.com.battlebits.ycommon.bukkit.BukkitCommon;
 import br.com.battlebits.ycommon.bukkit.BukkitMain;
 import br.com.battlebits.ycommon.bukkit.networking.PacketSender;
-import br.com.battlebits.ycommon.bungee.networking.CommonServer;
-import br.com.battlebits.ycommon.common.BattlebitsAPI;
 import br.com.battlebits.ycommon.common.networking.packets.CPacketAccountRequest;
 
 public class BukkitAccount extends BukkitCommon {
@@ -40,34 +35,6 @@ public class BukkitAccount extends BukkitCommon {
 
 	public void loadPlayer(UUID uuid) throws Exception {
 		PacketSender.sendPacket(new CPacketAccountRequest(uuid), BukkitMain.getPlugin().getPacketHandler());
-		Socket socket = new Socket(CommonServer.ADDRESS, CommonServer.PORT);
-		BattlebitsAPI.debug("SOCKET > CONNECT");
-		DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-		DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-		outputStream.writeUTF("Account");
-		outputStream.writeUTF("Load");
-		outputStream.writeUTF(uuid.toString());
-		outputStream.flush();
-		BattlebitsAPI.debug("SOCKET > MESSAGE SENT");
-		BattlebitsAPI.debug("SOCKET > MESSAGE WAITING");
-		String command = inputStream.readUTF();
-		BattlebitsAPI.debug("SOCKET > MESSAGE RECEIVED");
-		if (command.equals("Account")) {
-			String json = inputStream.readUTF();
-			BukkitPlayer battlePlayer = BukkitMain.getGson().fromJson(json, BukkitPlayer.class);
-			BattlebitsAPI.getAccountCommon().loadBattlePlayer(uuid, battlePlayer);
-			BattlebitsAPI.debug("NEW BATTLEPLAYER > " + battlePlayer.getUserName() + " (" + uuid + ")");
-			battlePlayer = null;
-			json = null;
-		}
-		BattlebitsAPI.debug("SOCKET > CLOSE");
-		outputStream.close();
-		inputStream.close();
-		socket.close();
-		command = null;
-		outputStream = null;
-		inputStream = null;
-		socket = null;
 	}
 
 }
