58e7661337108af6f12d546cea89338a20309e66
flametuner <flame.tuner@gmail.com>
2016-03-03 02:02:28 -0300

    Registrados os Pacotes do yCommon

--- src/br/com/battlebits/ycommon/common/networking/CommonPacket.java
+++ src/br/com/battlebits/ycommon/common/networking/CommonPacket.java
@@ -6,7 +6,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 import br.com.battlebits.ycommon.common.networking.packets.CPacketAccountLoad;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketAccountRequest;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketAddBlockedPlayer;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketAddFriend;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketAddFriendRequest;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketAddGroup;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketAddRank;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketBanPlayer;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketChangeFichas;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketChangeLanguage;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketChangeLiga;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketChangeMoney;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketChangeXp;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketCreateParty;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketDisbandParty;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketJoinParty;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketRemoveBlockedPlayer;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketRemoveFriend;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketRemoveFriendRequest;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketRemoveGroup;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketRemoveRank;
 import br.com.battlebits.ycommon.common.networking.packets.CPacketTranslationsLoad;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketTranslationsRequest;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketUnbanPlayer;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketUpdateClan;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketUpdateGameStatus;
+import br.com.battlebits.ycommon.common.networking.packets.CPacketUpdateProfile;
 
 public abstract class CommonPacket {
 	private static final Map<Byte, Class<?>> MAP_CLASS = new HashMap<Byte, Class<?>>();
@@ -15,9 +40,50 @@ public abstract class CommonPacket {
 	private static final Map<Byte, Boolean> MAP_VALID = new HashMap<Byte, Boolean>();
 
 	static {
-		/** 0x0X: Load **/
-		register((byte) 0x00, CPacketAccountLoad.class);
-		register((byte) 0x01, CPacketTranslationsLoad.class);
+		/** 0x0X: Account **/
+		register((byte) 0x00, CPacketAccountRequest.class);
+		register((byte) 0x01, CPacketAccountLoad.class);
+
+		/** 0x1X: Translations **/
+		register((byte) 0x10, CPacketTranslationsRequest.class);
+		register((byte) 0x11, CPacketTranslationsLoad.class);
+
+		/** 0x2X: Change Profile **/
+		register((byte) 0x20, CPacketChangeLanguage.class);
+		register((byte) 0x21, CPacketChangeXp.class);
+		register((byte) 0x22, CPacketChangeFichas.class);
+		register((byte) 0x23, CPacketChangeLiga.class);
+		register((byte) 0x24, CPacketChangeMoney.class);
+
+		/** 0x3X: Update **/
+		register((byte) 0x31, CPacketUpdateGameStatus.class);
+		register((byte) 0x32, CPacketUpdateProfile.class);
+
+		/** 0x4X: Friends **/
+		register((byte) 0x40, CPacketAddFriend.class);
+		register((byte) 0x41, CPacketRemoveFriend.class);
+		register((byte) 0x42, CPacketAddFriendRequest.class);
+		register((byte) 0x43, CPacketRemoveFriendRequest.class);
+		register((byte) 0x44, CPacketAddBlockedPlayer.class);
+		register((byte) 0x45, CPacketRemoveBlockedPlayer.class);
+
+		/** 0x5X: Groups **/
+		register((byte) 0x50, CPacketAddGroup.class);
+		register((byte) 0x51, CPacketRemoveGroup.class);
+		register((byte) 0x52, CPacketAddRank.class);
+		register((byte) 0x53, CPacketRemoveRank.class);
+
+		/** 0x6X: Party **/
+		register((byte) 0x60, CPacketCreateParty.class);
+		register((byte) 0x61, CPacketJoinParty.class);
+		register((byte) 0x62, CPacketDisbandParty.class);
+
+		/** 0x7X: Clans **/
+		register((byte) 0x70, CPacketUpdateClan.class);
+
+		/** 0x8X: Bans **/
+		register((byte) 0x80, CPacketBanPlayer.class);
+		register((byte) 0x81, CPacketUnbanPlayer.class);
 	}
 
 	private static void register(byte i, Class<? extends CommonPacket> c, short v) {
---
+++ src/br/com/battlebits/ycommon/common/networking/packets/CPacketJoinParty.java
@@ -0,0 +1,26 @@
+package br.com.battlebits.ycommon.common.networking.packets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import br.com.battlebits.ycommon.common.networking.CommonHandler;
+import br.com.battlebits.ycommon.common.networking.CommonPacket;
+
+public class CPacketJoinParty extends CommonPacket {
+
+	@Override
+	public void read(DataInputStream in) throws Exception {
+
+	}
+
+	@Override
+	public void write(DataOutputStream out) throws Exception {
+
+	}
+
+	@Override
+	public void handle(CommonHandler handler) throws Exception {
+
+	}
+
+}
