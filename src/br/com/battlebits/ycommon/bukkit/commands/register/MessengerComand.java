0f95dc3a690654acb2f1bc858df9d3446b962b05
zViitoor <rodrigues.victor@outlook.com>
2016-03-26 00:57:07 -0300

    Testes com comandos e AccountConfiguration atualizado

--- src/br/com/battlebits/ycommon/bukkit/commands/register/MessengerComand.java
+++ src/br/com/battlebits/ycommon/bukkit/commands/register/MessengerComand.java
@@ -1,5 +1,27 @@
 package br.com.battlebits.ycommon.bukkit.commands.register;
 
+import br.com.battlebits.ycommon.bukkit.accounts.BukkitPlayer;
+import br.com.battlebits.ycommon.bukkit.commands.CommandFramework.Command;
+import br.com.battlebits.ycommon.bukkit.commands.CommandFramework.CommandArgs;
+import br.com.battlebits.ycommon.common.BattlebitsAPI;
+
 public class MessengerComand {
+
+	@Command(name = "tell")
+	public void tell(CommandArgs args) {
+		if (args.isPlayer()) {
+			BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(args.getPlayer().getUniqueId());
+			if (args.getArgs().length == 0) {
+				args.getPlayer().sendMessage("Seu tell está " + player.getConfiguration().isTellEnabled());
+			} else {
+				String what = args.getArgs()[0];
+				if(what.equalsIgnoreCase("off")){
+					player.getConfiguration().setTellEnabled(false);
+				} else {
+					player.getConfiguration().setTellEnabled(true);
+				}
+			}
+		}
+	}
 
 }
--- src/br/com/battlebits/ycommon/common/account/AccountConfiguration.java
+++ src/br/com/battlebits/ycommon/common/account/AccountConfiguration.java
@@ -4,9 +4,25 @@ public class AccountConfiguration {
 
 	private boolean ignoreAll;
 	private boolean tellEnabled;
-	
+
 	@Override
 	public String toString() {
 		return super.toString();
+	}
+
+	public boolean isIgnoreAll() {
+		return ignoreAll;
+	}
+
+	public void setIgnoreAll(boolean ignoreAll) {
+		this.ignoreAll = ignoreAll;
+	}
+
+	public boolean isTellEnabled() {
+		return tellEnabled;
+	}
+
+	public void setTellEnabled(boolean tellEnabled) {
+		this.tellEnabled = tellEnabled;
 	}
 }
