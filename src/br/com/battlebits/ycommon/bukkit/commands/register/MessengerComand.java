ce990229ff4cb1b590437b9cb1c03ae41a6daaac
zViitoor <rodrigues.victor@outlook.com>
2016-04-08 09:41:10 -0300

    Agora, o sistema de tell está compativel com o sistema de bloquear

--- src/br/com/battlebits/ycommon/bukkit/commands/register/MessengerComand.java
+++ src/br/com/battlebits/ycommon/bukkit/commands/register/MessengerComand.java
@@ -36,49 +36,58 @@ public class MessengerComand extends CommandClass {
 					if (t != null) {
 						if (t.getUniqueId() != p.getUniqueId()) {
 							BukkitPlayer bt = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(t.getUniqueId());
-							if (bt.getConfiguration().isTellEnabled()) {
-								TextComponent[] toPlayer = new TextComponent[args.getArgs().length];
-								TextComponent to = new TextComponent(
-										Translate.getTranslation(bp.getLanguage(), "tell-me-player").replace("%player%", t.getName()));
-								to.setClickEvent(
-										new ClickEvent(Action.SUGGEST_COMMAND, "/" + args.getLabel().toLowerCase() + " " + t.getName() + " "));
-								to.setHoverEvent(new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT,
-										new TextComponent[] { new TextComponent(Translate.getTranslation(bp.getLanguage(), "tell-hover-another")
-												.replace("%player%", t.getName())) }));
-								toPlayer[0] = to;
-								to = null;
-								TextComponent[] toTarget = new TextComponent[args.getArgs().length];
-								TextComponent from = new TextComponent(
-										Translate.getTranslation(bt.getLanguage(), "tell-player-me").replace("%player%", p.getName()));
-								from.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/r "));
-								from.setHoverEvent(
-										new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT, new TextComponent[] { new TextComponent(
-												Translate.getTranslation(bt.getLanguage(), "tell-hover-reply").replace("%player%", p.getName())) }));
-								toTarget[0] = from;
-								from = null;
-								for (int i = 1; i < args.getArgs().length; i += 1) {
-									String msg = args.getArgs()[i];
-									msg = " " + msg;
-									TextComponent text = new TextComponent(msg);
-									List<String> url = StringURLUtils.extractUrls(msg);
-									if (url.size() > 0) {
-										text.setClickEvent(new ClickEvent(Action.OPEN_URL, url.get(0)));
+							if (!bt.getConfiguration().isIgnoreAll()) {
+								if (!bt.getBlockedPlayers().containsKey(t.getUniqueId())) {
+									if (bt.getConfiguration().isTellEnabled()) {
+										TextComponent[] toPlayer = new TextComponent[args.getArgs().length];
+										TextComponent to = new TextComponent(
+												Translate.getTranslation(bp.getLanguage(), "tell-me-player").replace("%player%", t.getName()));
+										to.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND,
+												"/" + args.getLabel().toLowerCase() + " " + t.getName() + " "));
+										to.setHoverEvent(new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT,
+												new TextComponent[] { new TextComponent(Translate
+														.getTranslation(bp.getLanguage(), "tell-hover-another").replace("%player%", t.getName())) }));
+										toPlayer[0] = to;
+										to = null;
+										TextComponent[] toTarget = new TextComponent[args.getArgs().length];
+										TextComponent from = new TextComponent(
+												Translate.getTranslation(bt.getLanguage(), "tell-player-me").replace("%player%", p.getName()));
+										from.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/r "));
+										from.setHoverEvent(new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT,
+												new TextComponent[] { new TextComponent(Translate.getTranslation(bt.getLanguage(), "tell-hover-reply")
+														.replace("%player%", p.getName())) }));
+										toTarget[0] = from;
+										from = null;
+										for (int i = 1; i < args.getArgs().length; i += 1) {
+											String msg = args.getArgs()[i];
+											msg = " " + msg;
+											TextComponent text = new TextComponent(msg);
+											List<String> url = StringURLUtils.extractUrls(msg);
+											if (url.size() > 0) {
+												text.setClickEvent(new ClickEvent(Action.OPEN_URL, url.get(0)));
+											}
+											toPlayer[i] = text;
+											toTarget[i] = text;
+											text = null;
+											url = null;
+											msg = null;
+										}
+										p.spigot().sendMessage(toPlayer);
+										t.spigot().sendMessage(toTarget);
+										// TODO: SEND MESSAGE ON
+										// BOSSBAR/ACTIONBAR
+										// TODO: PLAY SOUND
+										bt.setLastTellUUID(p.getUniqueId());
+										toTarget = null;
+										toPlayer = null;
+									} else {
+										p.sendMessage(Translate.getTranslation(bp.getLanguage(), "tell-player-disabled"));
 									}
-									toPlayer[i] = text;
-									toTarget[i] = text;
-									text = null;
-									url = null;
-									msg = null;
+								} else {
+									p.sendMessage(Translate.getTranslation(bp.getLanguage(), "tell-ignore-you"));
 								}
-								p.spigot().sendMessage(toPlayer);
-								t.spigot().sendMessage(toTarget);
-								// TODO: SEND MESSAGE ON BOSSBAR/ACTIONBAR
-								// TODO: PLAY SOUND
-								bt.setLastTellUUID(p.getUniqueId());
-								toTarget = null;
-								toPlayer = null;
 							} else {
-								p.sendMessage(Translate.getTranslation(bp.getLanguage(), "tell-player-disabled"));
+								p.sendMessage(Translate.getTranslation(bp.getLanguage(), "tell-ignore-all"));
 							}
 							bt = null;
 						} else {
@@ -109,48 +118,56 @@ public class MessengerComand extends CommandClass {
 					Player t = Bukkit.getPlayer(bp.getLastTellUUID());
 					if (t != null) {
 						BukkitPlayer bt = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(t.getUniqueId());
-						if (bt.getConfiguration().isTellEnabled()) {
-							TextComponent[] toPlayer = new TextComponent[args.getArgs().length + 1];
-							TextComponent to = new TextComponent(
-									Translate.getTranslation(bp.getLanguage(), "tell-me-player").replace("%player%", t.getName()));
-							to.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/tell " + t.getName() + " "));
-							to.setHoverEvent(
-									new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT, new TextComponent[] { new TextComponent(
-											Translate.getTranslation(bp.getLanguage(), "tell-hover-another").replace("%player%", t.getName())) }));
-							toPlayer[0] = to;
-							to = null;
-							TextComponent[] toTarget = new TextComponent[args.getArgs().length + 1];
-							TextComponent from = new TextComponent(
-									Translate.getTranslation(bt.getLanguage(), "tell-player-me").replace("%player%", p.getName()));
-							from.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/r "));
-							from.setHoverEvent(
-									new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT, new TextComponent[] { new TextComponent(
-											Translate.getTranslation(bt.getLanguage(), "tell-hover-reply").replace("%player%", p.getName())) }));
-							toTarget[0] = from;
-							from = null;
-							for (int i = 0; i < args.getArgs().length; i += 1) {
-								String msg = args.getArgs()[i];
-								msg = " " + msg;
-								TextComponent text = new TextComponent(msg);
-								List<String> url = StringURLUtils.extractUrls(msg);
-								if (url.size() > 0) {
-									text.setClickEvent(new ClickEvent(Action.OPEN_URL, url.get(0)));
+						if (!bt.getConfiguration().isIgnoreAll()) {
+							if (!bt.getBlockedPlayers().containsKey(t.getUniqueId())) {
+								if (bt.getConfiguration().isTellEnabled()) {
+									TextComponent[] toPlayer = new TextComponent[args.getArgs().length + 1];
+									TextComponent to = new TextComponent(
+											Translate.getTranslation(bp.getLanguage(), "tell-me-player").replace("%player%", t.getName()));
+									to.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/tell " + t.getName() + " "));
+									to.setHoverEvent(new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT,
+											new TextComponent[] { new TextComponent(Translate.getTranslation(bp.getLanguage(), "tell-hover-another")
+													.replace("%player%", t.getName())) }));
+									toPlayer[0] = to;
+									to = null;
+									TextComponent[] toTarget = new TextComponent[args.getArgs().length + 1];
+									TextComponent from = new TextComponent(
+											Translate.getTranslation(bt.getLanguage(), "tell-player-me").replace("%player%", p.getName()));
+									from.setClickEvent(new ClickEvent(Action.SUGGEST_COMMAND, "/r "));
+									from.setHoverEvent(new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT,
+											new TextComponent[] { new TextComponent(Translate.getTranslation(bt.getLanguage(), "tell-hover-reply")
+													.replace("%player%", p.getName())) }));
+									toTarget[0] = from;
+									from = null;
+									for (int i = 0; i < args.getArgs().length; i += 1) {
+										String msg = args.getArgs()[i];
+										msg = " " + msg;
+										TextComponent text = new TextComponent(msg);
+										List<String> url = StringURLUtils.extractUrls(msg);
+										if (url.size() > 0) {
+											text.setClickEvent(new ClickEvent(Action.OPEN_URL, url.get(0)));
+										}
+										toPlayer[i + 1] = text;
+										toTarget[i + 1] = text;
+										text = null;
+										url = null;
+										msg = null;
+									}
+									p.spigot().sendMessage(toPlayer);
+									t.spigot().sendMessage(toTarget);
+									// TODO: SEND MESSAGE ON BOSSBAR/ACTIONBAR
+									// TODO: PLAY SOUND
+									bt.setLastTellUUID(p.getUniqueId());
+									toTarget = null;
+									toPlayer = null;
+								} else {
+									p.sendMessage(Translate.getTranslation(bp.getLanguage(), "reply-tell-off"));
 								}
-								toPlayer[i + 1] = text;
-								toTarget[i + 1] = text;
-								text = null;
-								url = null;
-								msg = null;
+							} else {
+								p.sendMessage(Translate.getTranslation(bp.getLanguage(), "tell-ignore-you"));
 							}
-							p.spigot().sendMessage(toPlayer);
-							t.spigot().sendMessage(toTarget);
-							// TODO: SEND MESSAGE ON BOSSBAR/ACTIONBAR
-							// TODO: PLAY SOUND
-							bt.setLastTellUUID(p.getUniqueId());
-							toTarget = null;
-							toPlayer = null;
 						} else {
-							p.sendMessage(Translate.getTranslation(bp.getLanguage(), "reply-tell-off"));
+							p.sendMessage(Translate.getTranslation(bp.getLanguage(), "tell-ignore-all"));
 						}
 						bt = null;
 					} else {
@@ -199,7 +216,8 @@ public class MessengerComand extends CommandClass {
 											Translate.getTranslation(bp.getLanguage(), "ignore-you-ignore").replace("%player%", args.getArgs()[0]));
 								} else {
 									bp.getBlockedPlayers().remove(id);
-									p.sendMessage(Translate.getTranslation(bp.getLanguage(), "ignore-not-ignore").replace("%player%", args.getArgs()[0]));
+									p.sendMessage(
+											Translate.getTranslation(bp.getLanguage(), "ignore-not-ignore").replace("%player%", args.getArgs()[0]));
 								}
 							} else {
 								p.sendMessage(Translate.getTranslation(bp.getLanguage(), "ignore-cant-you"));
