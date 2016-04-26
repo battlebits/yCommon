7596fcb38d887631707d29d94221ca1b9283a65e
flametuner <flame.tuner@gmail.com>
2016-04-25 22:28:28 -0300

    Adicionado remoção de network

--- src/br/com/battlebits/ycommon/bungee/commands/register/GroupCommand.java
+++ src/br/com/battlebits/ycommon/bungee/commands/register/GroupCommand.java
@@ -78,6 +78,8 @@ public class GroupCommand extends CommandClass {
 				}
 
 				if (group == Group.NORMAL) {
+					if (player.getGroups().containsKey(ServerType.NETWORK) && battleSender.getServerGroup() == Group.DONO)
+						serverType = ServerType.NETWORK;
 					player.getGroups().remove(serverType);
 				} else {
 					player.getGroups().put(serverType, group);
