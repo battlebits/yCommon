6174650a4fa88d64a81fbc4f40857cd03aaf1a48
flametuner <flame.tuner@gmail.com>
2016-04-28 16:26:39 -0300

    Arrumado ID e adicionado debug

--- src/br/com/battlebits/ycommon/bukkit/listeners/TagListener.java
+++ src/br/com/battlebits/ycommon/bukkit/listeners/TagListener.java
@@ -6,10 +6,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import br.com.battlebits.ycommon.bukkit.BukkitMain;
 import br.com.battlebits.ycommon.bukkit.accounts.BukkitPlayer;
 import br.com.battlebits.ycommon.bukkit.event.account.update.PlayerChangeTagEvent;
+import br.com.battlebits.ycommon.bukkit.tagmanager.TagManager;
 import br.com.battlebits.ycommon.common.BattlebitsAPI;
 import br.com.battlebits.ycommon.common.enums.Liga;
 import br.com.battlebits.ycommon.common.tag.Tag;
@@ -25,55 +27,44 @@ public class TagListener implements Listener {
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerJoinListener(PlayerJoinEvent e) {
-		// new BukkitRunnable() {
-		// @SuppressWarnings("deprecation")
-		// @Override
-		// public void run() {
-		Player p = e.getPlayer();
-		BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
-		String id = getTeamName(player.getTag(), player.getLiga());
-		main.getBattleScoreboard().createTeam(e.getPlayer(), id, player.getTag().getPrefix(player.getLanguage()) + " ",
-				" §7(" + player.getLiga().getSymbol() + "§7)");
-		main.getBattleScoreboard().joinTeam(p, p, player.getTag().ordinal() + "-" + player.getLiga().ordinal());
-		for (Player o : Bukkit.getOnlinePlayers()) {
-			if (o.getPlayer().getUniqueId() != e.getPlayer().getUniqueId()) {
-				BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
-				main.getBattleScoreboard().createTeam(e.getPlayer(), getTeamName(bp.getTag(), bp.getLiga()),
-						bp.getTag().getPrefix(player.getLanguage()) + " ", " §7(" + bp.getLiga().getSymbol() + "§7)");
-				main.getBattleScoreboard().joinTeam(e.getPlayer(), o, (bp.getTag().ordinal()) + "-" + (bp.getLiga().ordinal()));
-				main.getBattleScoreboard().createTeam(o, id, player.getTag().getPrefix(bp.getLanguage()) + " ",
-						" §7(" + player.getLiga().getSymbol() + "§7)");
-				main.getBattleScoreboard().joinTeam(o, p,
-						(Tag.values().length - bp.getTag().ordinal()) + "-" + (Liga.values().length - bp.getLiga().ordinal()));
-				bp = null;
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				Player p = e.getPlayer();
+				BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
+				player.setTag(TagManager.getPlayerDefaultTag(player));
+				String id = getTeamName(player.getTag(), player.getLiga());
+				for (Player o : Bukkit.getOnlinePlayers()) {
+					if (o.getPlayer().getUniqueId() != e.getPlayer().getUniqueId()) {
+						BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
+						String id2 = getTeamName(bp.getTag(), bp.getLiga());
+						main.getBattleScoreboard().createTeam(e.getPlayer(), id2, bp.getTag().getPrefix(player.getLanguage()) + " ", " §7(" + bp.getLiga().getSymbol() + "§7)");
+						main.getBattleScoreboard().joinTeam(e.getPlayer(), o, id2);
+						main.getBattleScoreboard().createTeam(o, id, player.getTag().getPrefix(bp.getLanguage()) + " ", " §7(" + player.getLiga().getSymbol() + "§7)");
+						main.getBattleScoreboard().joinTeam(o, p, id);
+						bp = null;
+					}
+					o = null;
+				}
+				id = null;
+				player = null;
+				p = null;
 			}
-			o = null;
-		}
-		id = null;
-		player = null;
-		p = null;
-		// }
-		// }.runTaskAsynchronously(main);
+		}.runTaskAsynchronously(main);
 	}
 
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerChangeTagListener(PlayerChangeTagEvent e) {
-		// new BukkitRunnable() {
-		// @SuppressWarnings("deprecation")
-		// @Override
-		// public void run() {
 		Player p = e.getPlayer();
 		BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
 		String id = getTeamName(e.getNewTag(), player.getLiga());
-		main.getBattleScoreboard().createTeam(p, id, e.getNewTag().getPrefix(player.getLanguage()) + " ",
-				" §7(" + player.getLiga().getSymbol() + "§7)");
+		main.getBattleScoreboard().createTeam(p, id, e.getNewTag().getPrefix(player.getLanguage()) + " ", " §7(" + player.getLiga().getSymbol() + "§7)");
 		main.getBattleScoreboard().joinTeam(p, p, id);
 		for (Player o : Bukkit.getOnlinePlayers()) {
 			if (p.getUniqueId() != o.getUniqueId()) {
 				BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
-				main.getBattleScoreboard().createTeam(o, id, e.getNewTag().getPrefix(bp.getLanguage()) + " ",
-						" §7(" + player.getLiga().getSymbol() + "§7)");
+				main.getBattleScoreboard().createTeam(o, id, e.getNewTag().getPrefix(bp.getLanguage()) + " ", " §7(" + player.getLiga().getSymbol() + "§7)");
 				main.getBattleScoreboard().joinTeam(o, p, id);
 				bp = null;
 			}
@@ -82,20 +73,18 @@ public class TagListener implements Listener {
 		id = null;
 		player = null;
 		p = null;
-		// }
-		// }.runTaskAsynchronously(main);
 	}
 
-	private char[] chars = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-			'w', 'x', 'y', 'z' };
+	private static char[] chars = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-	public String getTeamName(Tag tag, Liga liga) {
+	public static String getTeamName(Tag tag, Liga liga) {
 		return chars
 				[tag
 				 .ordinal()] 
-						 + "-" + 
-				 chars[liga.
-				       ordinal()];
+						 + "-" 
+				 + chars
+				 [liga
+				  .ordinal()];
 	}
 
 }
