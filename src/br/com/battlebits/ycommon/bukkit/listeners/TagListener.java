64f9a8ceb76e96020f2d133400210dc34cda1d73
zViitoor <rodrigues.victor@outlook.com>
2016-04-28 16:05:43 -0300

    TeamID com letras

--- src/br/com/battlebits/ycommon/bukkit/listeners/TagListener.java
+++ src/br/com/battlebits/ycommon/bukkit/listeners/TagListener.java
@@ -25,65 +25,71 @@ public class TagListener implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerJoinListener(PlayerJoinEvent e) {
-		new BukkitRunnable() {
-			@SuppressWarnings("deprecation")
-			@Override
-			public void run() {
-				Player p = e.getPlayer();
-				BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
-				String id = (Tag.values().length - player.getTag().ordinal()) + "-" + (Liga.values().length - player.getLiga().ordinal());
-				main.getBattleScoreboard().createTeam(e.getPlayer(), id, player.getTag().getPrefix(player.getLanguage()) + " ",
+		// new BukkitRunnable() {
+		// @SuppressWarnings("deprecation")
+		// @Override
+		// public void run() {
+		Player p = e.getPlayer();
+		BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
+		String id = getTeamName(player.getTag(), player.getLiga());
+		main.getBattleScoreboard().createTeam(e.getPlayer(), id, player.getTag().getPrefix(player.getLanguage()) + " ",
+				" §7(" + player.getLiga().getSymbol() + "§7)");
+		main.getBattleScoreboard().joinTeam(p, p, player.getTag().ordinal() + "-" + player.getLiga().ordinal());
+		for (Player o : Bukkit.getOnlinePlayers()) {
+			if (o.getPlayer().getUniqueId() != e.getPlayer().getUniqueId()) {
+				BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
+				main.getBattleScoreboard().createTeam(e.getPlayer(), getTeamName(bp.getTag(), bp.getLiga()),
+						bp.getTag().getPrefix(player.getLanguage()) + " ", " §7(" + bp.getLiga().getSymbol() + "§7)");
+				main.getBattleScoreboard().joinTeam(e.getPlayer(), o, (bp.getTag().ordinal()) + "-" + (bp.getLiga().ordinal()));
+				main.getBattleScoreboard().createTeam(o, id, player.getTag().getPrefix(bp.getLanguage()) + " ",
 						" §7(" + player.getLiga().getSymbol() + "§7)");
-				main.getBattleScoreboard().joinTeam(p, p, player.getTag().ordinal() + "-" + player.getLiga().ordinal());
-				for (Player o : Bukkit.getOnlinePlayers()) {
-					if (o.getPlayer().getUniqueId() != e.getPlayer().getUniqueId()) {
-						BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
-						main.getBattleScoreboard().createTeam(e.getPlayer(), bp.getTag().ordinal() + "-" + bp.getLiga().ordinal(),
-								bp.getTag().getPrefix(player.getLanguage()) + " ", " §7(" + bp.getLiga().getSymbol() + "§7)");
-						main.getBattleScoreboard().joinTeam(e.getPlayer(), o,
-								(Tag.values().length - bp.getTag().ordinal()) + "-" + (Liga.values().length - bp.getLiga().ordinal()));
-						main.getBattleScoreboard().createTeam(o, id, player.getTag().getPrefix(bp.getLanguage()) + " ",
-								" §7(" + player.getLiga().getSymbol() + "§7)");
-						main.getBattleScoreboard().joinTeam(o, p,
-								(Tag.values().length - bp.getTag().ordinal()) + "-" + (Liga.values().length - bp.getLiga().ordinal()));
-						bp = null;
-					}
-					o = null;
-				}
-				id = null;
-				player = null;
-				p = null;
+				main.getBattleScoreboard().joinTeam(o, p,
+						(Tag.values().length - bp.getTag().ordinal()) + "-" + (Liga.values().length - bp.getLiga().ordinal()));
+				bp = null;
 			}
-		}.runTaskAsynchronously(main);
+			o = null;
+		}
+		id = null;
+		player = null;
+		p = null;
+		// }
+		// }.runTaskAsynchronously(main);
 	}
 
 	@EventHandler
 	public void onPlayerChangeTagListener(PlayerChangeTagEvent e) {
-		new BukkitRunnable() {
-			@SuppressWarnings("deprecation")
-			@Override
-			public void run() {
-				Player p = e.getPlayer();
-				BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
-				String id = (Tag.values().length - e.getNewTag().ordinal()) + "-" + (Liga.values().length - player.getLiga().ordinal());
-				main.getBattleScoreboard().createTeam(p, id, e.getNewTag().getPrefix(player.getLanguage()) + " ",
+		// new BukkitRunnable() {
+		// @SuppressWarnings("deprecation")
+		// @Override
+		// public void run() {
+		Player p = e.getPlayer();
+		BukkitPlayer player = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(e.getPlayer().getUniqueId());
+		String id = getTeamName(e.getNewTag(), player.getLiga());
+		main.getBattleScoreboard().createTeam(p, id, e.getNewTag().getPrefix(player.getLanguage()) + " ",
+				" §7(" + player.getLiga().getSymbol() + "§7)");
+		main.getBattleScoreboard().joinTeam(p, p, id);
+		for (Player o : Bukkit.getOnlinePlayers()) {
+			if (p.getUniqueId() != o.getUniqueId()) {
+				BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
+				main.getBattleScoreboard().createTeam(o, id, e.getNewTag().getPrefix(bp.getLanguage()) + " ",
 						" §7(" + player.getLiga().getSymbol() + "§7)");
-				main.getBattleScoreboard().joinTeam(p, p, id);
-				for (Player o : Bukkit.getOnlinePlayers()) {
-					if (p.getUniqueId() != o.getUniqueId()) {
-						BukkitPlayer bp = (BukkitPlayer) BattlebitsAPI.getAccountCommon().getBattlePlayer(o.getUniqueId());
-						main.getBattleScoreboard().createTeam(o, id, e.getNewTag().getPrefix(bp.getLanguage()) + " ",
-								" §7(" + player.getLiga().getSymbol() + "§7)");
-						main.getBattleScoreboard().joinTeam(o, p, id);
-						bp = null;
-					}
-					o = null;
-				}
-				id = null;
-				player = null;
-				p = null;
+				main.getBattleScoreboard().joinTeam(o, p, id);
+				bp = null;
 			}
-		}.runTaskAsynchronously(main);
+			o = null;
+		}
+		id = null;
+		player = null;
+		p = null;
+		// }
+		// }.runTaskAsynchronously(main);
+	}
+
+	private char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			'y', 'z' };
+
+	public String getTeamName(Tag tag, Liga liga) {
+		return chars[tag.ordinal()] + "-" + chars[liga.ordinal()];
 	}
 
 }
