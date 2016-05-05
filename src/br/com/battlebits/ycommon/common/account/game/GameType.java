939eccbef849f61576fc560952f9640d9d36ec0f
flametuner <flame.tuner@gmail.com>
2016-05-04 22:12:40 -0300

    Adicionado GameType raid

--- src/br/com/battlebits/ycommon/common/account/game/GameType.java
+++ src/br/com/battlebits/ycommon/common/account/game/GameType.java
@@ -1,14 +1,15 @@
 package br.com.battlebits.ycommon.common.account.game;
 
 public enum GameType {
-	BATTLECRAFT_PVP_STATUS("bc_pvp_status");
-	
+	BATTLECRAFT_PVP_STATUS("bc_pvp_status"), //
+	RAID_ACCOUNT("raid_account");
+
 	private String serverId;
-	
+
 	private GameType(String str) {
 		serverId = str;
 	}
-	
+
 	public String getServerId() {
 		return serverId;
 	}
