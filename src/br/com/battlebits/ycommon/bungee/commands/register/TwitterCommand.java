30fe6e49240fdb6cd750e4a9473a27de1b56bf9d
flametuner <flame.tuner@gmail.com>
2016-05-16 18:18:07 -0300

    TwitterCommand

--- src/br/com/battlebits/ycommon/bungee/commands/register/TwitterCommand.java
+++ src/br/com/battlebits/ycommon/bungee/commands/register/TwitterCommand.java
@@ -8,12 +8,10 @@ import br.com.battlebits.ycommon.common.commands.CommandClass;
 import br.com.battlebits.ycommon.common.permissions.enums.Group;
 import br.com.battlebits.ycommon.common.translate.Translate;
 import br.com.battlebits.ycommon.common.translate.languages.Language;
+import br.com.battlebits.ycommon.common.twitter.TweetUtils;
+import br.com.battlebits.ycommon.common.twitter.TwitterAccount;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterCommand extends CommandClass {
 
@@ -24,7 +22,7 @@ public class TwitterCommand extends CommandClass {
 			language = BattlebitsAPI.getAccountCommon().getBattlePlayer(cmdArgs.getPlayer().getUniqueId()).getLanguage();
 		}
 		String[] args = cmdArgs.getArgs();
-		if(args.length <= 0) {
+		if (args.length <= 0) {
 			cmdArgs.getSender().sendMessage(TextComponent.fromLegacyText(Translate.getTranslation(language, "command-broadcast-usage").replace("%command%", cmdArgs.getLabel())));
 			return;
 		}
@@ -41,18 +39,8 @@ public class TwitterCommand extends CommandClass {
 		final Language lang = language;
 		BungeeMain.getPlugin().getProxy().getScheduler().runAsync(BungeeMain.getPlugin(), new Runnable() {
 			public void run() {
-				String consumerKey = "Y2lYtwoymGslYCVakGK1M0nvv";
-				String consumerSecret = "OcjNWd1wzN1AVEGjKAhd1e88otlfB5DJ60FNINSEuzmFIXzGlc";
-				String accessToken = "2972745022-92JuPoZ7dkHzc0YMDJusC17gcRO8qjyjeTwa6CW";
-				String accessSecret = "iLGnslOn8Lh9sQY28l4uIsnLCceRRAYZzcPy03AzRJ3Ji";
-				ConfigurationBuilder cb = new ConfigurationBuilder();
-				cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret).setOAuthAccessToken(accessToken).setOAuthAccessTokenSecret(accessSecret);
-				Twitter twitter = new TwitterFactory(cb.build()).getInstance();
-				try {
-					twitter.updateStatus(builder.toString());
+				if (TweetUtils.tweet(TwitterAccount.BATTLEBITSMC, builder.toString())) {
 					cmdArgs.getSender().sendMessage(TextComponent.fromLegacyText(Translate.getTranslation(lang, "tweet-success").replace("%message%", builder.toString())));
-				} catch (TwitterException e) {
-					e.printStackTrace();
 				}
 			}
 		});
