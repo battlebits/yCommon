b90e710ec58b158d2db7696125155e45834f349b
flametuner <flame.tuner@gmail.com>
2016-05-31 18:26:58 -0300

    Comandos /fake e /changeskin feitos

--- src/br/com/battlebits/ycommon/bukkit/commands/register/YoutubeCommand.java
+++ src/br/com/battlebits/ycommon/bukkit/commands/register/YoutubeCommand.java
@@ -1,7 +1,93 @@
 package br.com.battlebits.ycommon.bukkit.commands.register;
 
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import br.com.battlebits.ycommon.bukkit.BukkitMain;
+import br.com.battlebits.ycommon.bukkit.commands.BukkitCommandFramework.Command;
+import br.com.battlebits.ycommon.bukkit.commands.BukkitCommandFramework.CommandArgs;
+import br.com.battlebits.ycommon.bukkit.utils.FakePlayerUtils;
+import br.com.battlebits.ycommon.common.BattlebitsAPI;
+import br.com.battlebits.ycommon.common.account.BattlePlayer;
 import br.com.battlebits.ycommon.common.commandmanager.CommandClass;
+import br.com.battlebits.ycommon.common.permissions.enums.Group;
+import br.com.battlebits.ycommon.common.translate.Translate;
 
 public class YoutubeCommand extends CommandClass {
+
+	@Command(name = "fake", groupToUse = Group.YOUTUBER, noPermMessageId = "command-fake-no-access", runAsync = true)
+	public void fake(CommandArgs args) {
+		if (!args.isPlayer()) {
+			args.getSender().sendMessage("COMANDO PARA PLAYERS");
+			return;
+		}
+		Player p = args.getPlayer();
+		BattlePlayer bP = BattlebitsAPI.getAccountCommon().getBattlePlayer(p.getUniqueId());
+		if (!bP.getServerGroup().toString().contains("YOUTUBER") && !bP.hasGroupPermission(Group.STREAMER)) {
+			p.sendMessage(Translate.getTranslation(bP.getLanguage(), "command-fake-no-access"));
+			return;
+		}
+		String fakePrefix = Translate.getTranslation(bP.getLanguage(), "command-fake-prefix") + " ";
+		if (args.getArgs().length != 1) {
+			p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-fake-usage"));
+			return;
+		}
+		String playerName = args.getArgs()[0];
+		if (!FakePlayerUtils.validateName(playerName)) {
+			p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-fake-invalid"));
+			return;
+		}
+
+		if (BattlebitsAPI.getUUIDOf(playerName) != null) {
+			p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-fake-player-exists"));
+			return;
+		}
+
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				BukkitMain.getPlugin().getTagManager().removePlayerTag(p);
+				FakePlayerUtils.changePlayerName(p, playerName);
+				bP.setTag(bP.getTag());
+				p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-fake-changed-success"));
+			}
+		}.runTask(BukkitMain.getPlugin());
+	}
+
+	@Command(name = "changeskin", groupToUse = Group.ULTIMATE, noPermMessageId = "command-changeskin-no-access", runAsync = true)
+	public void changeskin(CommandArgs args) {
+		if (!args.isPlayer()) {
+			args.getSender().sendMessage("COMANDO PARA PLAYERS");
+			return;
+		}
+		Player p = args.getPlayer();
+		BattlePlayer bP = BattlebitsAPI.getAccountCommon().getBattlePlayer(p.getUniqueId());
+		String fakePrefix = Translate.getTranslation(bP.getLanguage(), "command-changeskin-prefix") + " ";
+		if (args.getArgs().length != 1) {
+			p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-changeskin-usage"));
+			return;
+		}
+		String playerName = args.getArgs()[0];
+		if (!FakePlayerUtils.validateName(playerName)) {
+			p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-changeskin-invalid"));
+			return;
+		}
+		UUID uuid = BattlebitsAPI.getUUIDOf(playerName);
+
+		if (uuid == null) {
+			p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-changeskin-player-not-exists"));
+			return;
+		}
+
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				FakePlayerUtils.changePlayerSkin(p, playerName, uuid);
+				p.sendMessage(fakePrefix + Translate.getTranslation(bP.getLanguage(), "command-changeskin-changed-success"));
+			}
+		}.runTask(BukkitMain.getPlugin());
+	}
 
 }
---
+++ src/br/com/battlebits/ycommon/bukkit/utils/FakePlayerUtils.java
@@ -0,0 +1,109 @@
+package br.com.battlebits.ycommon.bukkit.utils;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import com.mojang.authlib.GameProfile;
+
+import br.com.battlebits.ycommon.bukkit.BukkitMain;
+import br.com.battlebits.ycommon.bukkit.api.npc.CustomPlayerAPI;
+import net.minecraft.server.v1_7_R4.EntityPlayer;
+import net.minecraft.server.v1_7_R4.MathHelper;
+import net.minecraft.server.v1_7_R4.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_7_R4.PacketPlayOutEntityHeadRotation;
+import net.minecraft.server.v1_7_R4.PacketPlayOutEntityMetadata;
+import net.minecraft.server.v1_7_R4.PacketPlayOutNamedEntitySpawn;
+import net.minecraft.server.v1_7_R4.PacketPlayOutPlayerInfo;
+
+public class FakePlayerUtils {
+
+	public static void changePlayerName(Player player, String name) {
+		Collection<? extends Player> players = BukkitMain.getPlugin().getServer().getOnlinePlayers();
+		EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
+		GameProfile playerProfile = entityPlayer.getProfile();
+		removeFromTab(player, players);
+		try {
+			Field field = playerProfile.getClass().getDeclaredField("name");
+			field.setAccessible(true);
+			field.set(playerProfile, name);
+			field.setAccessible(false);
+			entityPlayer.getClass().getDeclaredField("displayName").set(entityPlayer, name);
+			entityPlayer.getClass().getDeclaredField("listName").set(entityPlayer, name);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		respawnPlayer(player, players);
+	}
+
+	public static void removePlayerSkin(Player player) {
+
+	}
+
+	public static void changePlayerSkin(Player player, String name, UUID uuid) {
+		Collection<? extends Player> players = BukkitMain.getPlugin().getServer().getOnlinePlayers();
+		EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
+		GameProfile playerProfile = entityPlayer.getProfile();
+		playerProfile.getProperties().clear();
+		try {
+			playerProfile.getProperties().put("textures", CustomPlayerAPI.Textures.get(new GameProfile(uuid, name)));
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		respawnPlayer(player, players);
+	}
+
+	public void addToTab(Player player, Collection<? extends Player> players) {
+		PacketPlayOutPlayerInfo addPlayerInfo = PacketPlayOutPlayerInfo.addPlayer(((CraftPlayer) player).getHandle());
+		PacketPlayOutPlayerInfo updatePlayerInfo = PacketPlayOutPlayerInfo.updateDisplayName(((CraftPlayer) player).getHandle());
+		for (Player online : players) {
+			if (!online.canSee(player))
+				continue;
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(addPlayerInfo);
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(updatePlayerInfo);
+		}
+	}
+
+	public static void removeFromTab(Player player, Collection<? extends Player> players) {
+		PacketPlayOutPlayerInfo removePlayerInfo = PacketPlayOutPlayerInfo.removePlayer(((CraftPlayer) player).getHandle());
+		for (Player online : players) {
+			if (!online.canSee(player))
+				continue;
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(removePlayerInfo);
+		}
+	}
+
+	public static void respawnPlayer(Player player, Collection<? extends Player> players) {
+		EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
+		PacketPlayOutEntityDestroy destroy = new PacketPlayOutEntityDestroy(entityPlayer.getId());
+		PacketPlayOutNamedEntitySpawn spawn = new PacketPlayOutNamedEntitySpawn(entityPlayer);
+		PacketPlayOutPlayerInfo addPlayerInfo = PacketPlayOutPlayerInfo.addPlayer(((CraftPlayer) player).getHandle());
+		PacketPlayOutEntityMetadata metadata = new PacketPlayOutEntityMetadata(entityPlayer.getId(), entityPlayer.getDataWatcher(), true);
+		PacketPlayOutEntityHeadRotation headRotation = new PacketPlayOutEntityHeadRotation(entityPlayer, (byte) MathHelper.d(entityPlayer.getHeadRotation() * 256.0F / 360.0F));
+		PacketPlayOutPlayerInfo removePlayerInfo = PacketPlayOutPlayerInfo.removePlayer(((CraftPlayer) player).getHandle());
+		for (Player online : players) {
+			if (!online.canSee(player))
+				continue;
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(removePlayerInfo);
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(addPlayerInfo);
+			if (online.getUniqueId() == player.getUniqueId())
+				continue;
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(destroy);
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(spawn);
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(metadata);
+			((CraftPlayer) online).getHandle().playerConnection.sendPacket(headRotation);
+		}
+	}
+
+	public static boolean validateName(String username) {
+		Pattern pattern = Pattern.compile("[a-zA-Z0-9_]{1,16}");
+		Matcher matcher = pattern.matcher(username);
+		return matcher.matches();
+	}
+}
