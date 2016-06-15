8fa0e311bad6789f581f08833f5a813cb43cb485
flametuner <flame.tuner@gmail.com>
2016-06-15 19:21:08 -0300

    Versão 1.0.15.4 lançada
    Arrumado comando de /fake

--- src/br/com/battlebits/ycommon/bukkit/utils/FakePlayerUtils.java
+++ src/br/com/battlebits/ycommon/bukkit/utils/FakePlayerUtils.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
 import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -15,17 +16,10 @@ import com.mojang.authlib.GameProfile;
 import br.com.battlebits.ycommon.bukkit.BukkitMain;
 import br.com.battlebits.ycommon.bukkit.api.npc.CustomPlayerAPI;
 import net.minecraft.server.v1_7_R4.EntityPlayer;
-import net.minecraft.server.v1_7_R4.EnumDifficulty;
-import net.minecraft.server.v1_7_R4.EnumGamemode;
-import net.minecraft.server.v1_7_R4.MathHelper;
-import net.minecraft.server.v1_7_R4.PacketPlayOutEntityDestroy;
-import net.minecraft.server.v1_7_R4.PacketPlayOutEntityHeadRotation;
-import net.minecraft.server.v1_7_R4.PacketPlayOutEntityMetadata;
-import net.minecraft.server.v1_7_R4.PacketPlayOutNamedEntitySpawn;
 import net.minecraft.server.v1_7_R4.PacketPlayOutPlayerInfo;
 import net.minecraft.server.v1_7_R4.PacketPlayOutPosition;
 import net.minecraft.server.v1_7_R4.PacketPlayOutRespawn;
-import net.minecraft.server.v1_7_R4.WorldType;
+import net.minecraft.server.v1_7_R4.WorldServer;
 
 public class FakePlayerUtils {
 
@@ -104,36 +98,31 @@ public class FakePlayerUtils {
 	}
 
 	public static void respawnPlayer(Player player, Collection<? extends Player> players) {
-		EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
-		PacketPlayOutEntityDestroy destroy = new PacketPlayOutEntityDestroy(entityPlayer.getId());
-		PacketPlayOutNamedEntitySpawn spawn = new PacketPlayOutNamedEntitySpawn(entityPlayer);
-		PacketPlayOutPlayerInfo addPlayerInfo = PacketPlayOutPlayerInfo.addPlayer(((CraftPlayer) player).getHandle());
-		PacketPlayOutEntityMetadata metadata = new PacketPlayOutEntityMetadata(entityPlayer.getId(), entityPlayer.getDataWatcher(), true);
-		PacketPlayOutEntityHeadRotation headRotation = new PacketPlayOutEntityHeadRotation(entityPlayer, (byte) MathHelper.d(entityPlayer.getHeadRotation() * 256.0F / 360.0F));
-		PacketPlayOutPlayerInfo removePlayerInfo = PacketPlayOutPlayerInfo.removePlayer(((CraftPlayer) player).getHandle());
+		respawnSelf(player);
 		for (Player online : players) {
-			if (!online.canSee(player))
-				continue;
-			((CraftPlayer) online).getHandle().playerConnection.sendPacket(removePlayerInfo);
-			((CraftPlayer) online).getHandle().playerConnection.sendPacket(addPlayerInfo);
-			if (online.getUniqueId() == player.getUniqueId()) {
-				respawnSelf(player);
+			if (online.equals(player) || !online.canSee(player)) {
 				continue;
 			}
-			((CraftPlayer) online).getHandle().playerConnection.sendPacket(destroy);
-			((CraftPlayer) online).getHandle().playerConnection.sendPacket(spawn);
-			((CraftPlayer) online).getHandle().playerConnection.sendPacket(metadata);
-			((CraftPlayer) online).getHandle().playerConnection.sendPacket(headRotation);
+			online.hidePlayer(player);
+			online.showPlayer(player);
 		}
 	}
 
 	@SuppressWarnings("deprecation")
 	public static void respawnSelf(Player player) {
-		PacketPlayOutRespawn respawn = new PacketPlayOutRespawn(player.getWorld().getEnvironment().getId(), EnumDifficulty.valueOf(player.getWorld().getDifficulty().toString()), WorldType.getType(player.getWorld().getWorldType().toString()), EnumGamemode.valueOf(player.getGameMode().toString()));
-		PacketPlayOutPosition position = new PacketPlayOutPosition(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), player.getLocation().getYaw(), player.getLocation().getPitch(), true);
 
-		((CraftPlayer) player).getHandle().playerConnection.sendPacket(respawn);
-		((CraftPlayer) player).getHandle().playerConnection.sendPacket(position);
+		EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
+		WorldServer server = ((CraftWorld) player.getWorld()).getHandle();
+		PacketPlayOutPlayerInfo addPlayerInfo = PacketPlayOutPlayerInfo.addPlayer(((CraftPlayer) player).getHandle());
+		PacketPlayOutPlayerInfo removePlayerInfo = PacketPlayOutPlayerInfo.removePlayer(((CraftPlayer) player).getHandle());
+		PacketPlayOutRespawn respawn = new PacketPlayOutRespawn(server.getWorld().getEnvironment().getId(), server.difficulty, server.getWorldData().getType(), entityPlayer.playerInteractManager.getGameMode());
+		PacketPlayOutPosition position = new PacketPlayOutPosition(entityPlayer.locX, entityPlayer.locY, entityPlayer.locZ, entityPlayer.yaw, entityPlayer.pitch, false);
+
+		entityPlayer.playerConnection.sendPacket(removePlayerInfo);
+		entityPlayer.playerConnection.sendPacket(addPlayerInfo);
+		entityPlayer.playerConnection.sendPacket(respawn);
+		entityPlayer.playerConnection.sendPacket(position);
+		player.updateInventory();
 	}
 
 	public static boolean validateName(String username) {
